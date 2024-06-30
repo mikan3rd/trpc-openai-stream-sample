26ca1e9db4d94d977daf84cf46c3176213d945f8
mikan3rd <ota.hiroki@bizforward.co.jp>
2024-06-30 20:04:23 +0900

    refactor: delete unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,19 @@
 import { trpc } from '../utils/trpc';
 import type { NextPageWithLayout } from './_app';
-import type { inferProcedureInput } from '@trpc/server';
 import Link from 'next/link';
-import { Fragment, useState } from 'react';
-import type { AppRouter } from '~/server/routers/_app';
+import { useState } from 'react';
 import { keepPreviousData } from '@tanstack/react-query';
 
 const IndexPage: NextPageWithLayout = () => {
-  const utils = trpc.useUtils();
-  const postsQuery = trpc.post.list.useInfiniteQuery(
-    {
-      limit: 5,
-    },
-    {
-      getNextPageParam(lastPage) {
-        return lastPage.nextCursor;
-      },
-    },
-  );
-
   // const iterable = trpc.examples.iterable.useQuery();
+
+  // const [iterableText, setIterableText] = useState<string>('');
+  // const iterable2 = trpc.examples.iterable2.useMutation();
 
   const [inputText, setInputText] = useState<string>(
     'ChatGPT-4の特徴を簡潔に説明してください',
   );
+
   const openai = trpc.examples.openai.useQuery(
     { text: inputText },
     {
@@ -31,10 +21,13 @@ const IndexPage: NextPageWithLayout = () => {
       placeholderData: keepPreviousData,
     },
   );
+  const submitByQuery = async () => {
+    openai.refetch();
+  };
 
   const [text, setText] = useState<string>('');
   const openai2 = trpc.examples.openai2.useMutation();
-  const submitText = async () => {
+  const submitByMutation = async () => {
     openai2.mutate(
       { text: inputText },
       {
@@ -45,31 +38,7 @@ const IndexPage: NextPageWithLayout = () => {
         },
       },
     );
-    // await openai.refetch();
   };
-
-  // const [posts, setPosts] = useState<any>(null);
-  // trpc.post.onPostAdd.useSubscription(undefined, {
-  //   onData(data) {
-  //     setPosts(data);
-  //   },
-  // });
-  // console.log({ posts });
-
-  const addPost = trpc.post.add.useMutation({
-    async onSuccess() {
-      // refetches posts after a post is added
-      await utils.post.list.invalidate();
-    },
-  });
-
-  // prefetch all posts for instant navigation
-  // useEffect(() => {
-  //   const allPosts = postsQuery.data?.pages.flatMap((page) => page.items) ?? [];
-  //   for (const { id } of allPosts) {
-  //     void utils.post.byId.prefetch({ id });
-  //   }
-  // }, [postsQuery.data, utils]);
 
   return (
     <div className="flex flex-col bg-gray-800 py-8">
@@ -109,132 +78,41 @@ const IndexPage: NextPageWithLayout = () => {
             />
 
             <div className="flex justify-center">
-              <input
+              <button
                 className="cursor-pointer bg-gray-900 p-2 rounded-md px-16"
                 type="submit"
                 onClick={(e) => {
                   e.preventDefault();
-                  submitText();
+                  submitByQuery();
                 }}
                 disabled={openai.isFetching}
-              />
-              {addPost.error && (
-                <p style={{ color: 'red' }}>{openai.error?.message}</p>
+              >
+                query
+              </button>
+              {openai.error && (
+                <p style={{ color: 'red' }}>{openai.error.message}</p>
               )}
-            </div>
-          </div>
-        </form>
 
-        <p className="py-4 break-all">{text}</p>
-      </div>
-
-      {/* <hr />
-
-      <div className="flex flex-col py-8 items-start gap-y-2">
-        <h2 className="text-3xl font-semibold">Iterable</h2>
-        <div>status={iterable.status}</div>
-        <div>isLoading={`${iterable.isLoading}`}</div>
-        <div>isFetching={`${iterable.isFetching}`}</div>
-        <div>data={iterable.data}</div>
-      </div> */}
-
-      <hr />
-
-      <div className="flex flex-col py-8 items-start gap-y-2">
-        <div className="flex flex-col"></div>
-        <h2 className="text-3xl font-semibold">
-          Latest Posts
-          {postsQuery.status === 'pending' && '(loading)'}
-        </h2>
-
-        <button
-          className="bg-gray-900 p-2 rounded-md font-semibold disabled:bg-gray-700 disabled:text-gray-400"
-          onClick={() => postsQuery.fetchNextPage()}
-          disabled={!postsQuery.hasNextPage || postsQuery.isFetchingNextPage}
-        >
-          {postsQuery.isFetchingNextPage
-            ? 'Loading more...'
-            : postsQuery.hasNextPage
-            ? 'Load More'
-            : 'Nothing more to load'}
-        </button>
-
-        {postsQuery.data?.pages.map((page, index) => (
-          <Fragment key={page.items[0]?.id ?? index}>
-            {page.items.map((item) => (
-              <article key={item.id}>
-                <h3 className="text-2xl font-semibold">{item.title}</h3>
-                <Link className="text-gray-400" href={`/post/${item.id}`}>
-                  View more
-                </Link>
-              </article>
-            ))}
-          </Fragment>
-        ))}
-      </div>
-
-      <hr />
-
-      <div className="flex flex-col py-8 items-center">
-        <h2 className="text-3xl font-semibold pb-2">Add a Post</h2>
-
-        <form
-          className="py-2 w-4/6"
-          onSubmit={async (e) => {
-            /**
-             * In a real app you probably don't want to use this manually
-             * Checkout React Hook Form - it works great with tRPC
-             * @link https://react-hook-form.com/
-             * @link https://kitchen-sink.trpc.io/react-hook-form
-             */
-            e.preventDefault();
-            const $form = e.currentTarget;
-            const values = Object.fromEntries(new FormData($form));
-            type Input = inferProcedureInput<AppRouter['post']['add']>;
-            //    ^?
-            const input: Input = {
-              title: values.title as string,
-              text: values.text as string,
-            };
-            try {
-              await addPost.mutateAsync(input);
-
-              $form.reset();
-            } catch (cause) {
-              console.error({ cause }, 'Failed to add post');
-            }
-          }}
-        >
-          <div className="flex flex-col gap-y-4 font-semibold">
-            <input
-              className="focus-visible:outline-dashed outline-offset-4 outline-2 outline-gray-700 rounded-xl px-4 py-3 bg-gray-900"
-              id="title"
-              name="title"
-              type="text"
-              placeholder="Title"
-              disabled={addPost.isPending}
-            />
-            <textarea
-              className="resize-none focus-visible:outline-dashed outline-offset-4 outline-2 outline-gray-700 rounded-xl px-4 py-3 bg-gray-900"
-              id="text"
-              name="text"
-              placeholder="Text"
-              disabled={addPost.isPending}
-              rows={6}
-            />
-
-            <div className="flex justify-center">
-              <input
+              <button
                 className="cursor-pointer bg-gray-900 p-2 rounded-md px-16"
                 type="submit"
-                disabled={addPost.isPending}
-              />
-              {addPost.error && (
-                <p style={{ color: 'red' }}>{addPost.error.message}</p>
+                onClick={(e) => {
+                  e.preventDefault();
+                  submitByMutation();
+                }}
+                disabled={openai2.isPending}
+              >
+                mutation
+              </button>
+              {openai2.error && (
+                <p style={{ color: 'red' }}>{openai2.error.message}</p>
               )}
             </div>
           </div>
         </form>
+
+        <p className="py-4 break-all">{openai.data}</p>
+        <p className="py-4 break-all">{text}</p>
       </div>
     </div>
   );
--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -2,9 +2,7 @@
  * This file contains the root router of your tRPC-backend
  */
 import { createCallerFactory, publicProcedure, router } from '../trpc';
-import { postRouter } from './post';
 import { z } from 'zod';
-import { sse } from '@trpc/server';
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
@@ -12,10 +10,6 @@ const openai = new OpenAI({
 });
 
 export const appRouter = router({
-  healthcheck: publicProcedure.query(() => 'yay!'),
-
-  post: postRouter,
-
   examples: {
     // HTTP Batch Stream Link
     // https://trpc.io/docs/client/links/httpBatchStreamLink
@@ -25,6 +19,13 @@ export const appRouter = router({
         yield i;
       }
     }),
+    iterable2: publicProcedure.mutation(async function* () {
+      for (let i = 0; i < 10; i++) {
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        yield i;
+      }
+    }),
+
     openai: publicProcedure
       .input(
         z.object({
@@ -74,42 +75,6 @@ export const appRouter = router({
           const target = chunk.choices[targetIndex];
           const content = target?.delta?.content ?? '';
           yield content;
-        }
-      }),
-  },
-
-  openai: {
-    chat: publicProcedure
-      .input(
-        z.object({
-          text: z.string().min(1),
-        }),
-      )
-      .subscription(async function* ({ input }) {
-        const stream = await openai.chat.completions.create({
-          model: 'gpt-3.5-turbo',
-          messages: [
-            {
-              role: 'user',
-              content: input.text,
-            },
-          ],
-          stream: true,
-        });
-        for await (const chunk of stream) {
-          const targetIndex = 0;
-          const target = chunk.choices[targetIndex];
-          const content = target?.delta?.content ?? '';
-
-          if (target?.index === undefined) {
-            throw new Error('missing index');
-          }
-
-          yield sse({
-            // yielding the post id ensures the client can reconnect at any time and get the latest events this id
-            id: target.index.toString(),
-            data: content,
-          });
         }
       }),
   },
